946cfa7e4b01942cf972974e874251018a1d3775
Firdosee <aafreenaafreen2395@gmail.com>
2022-02-08 20:54:46 +0530

    added logger changes

--- Pages/Base_Page.py
+++ Pages/Base_Page.py
@@ -15,13 +15,19 @@ class Base_Page:
             WebDriverWait(self.driver, 30).until(expected_conditions.presence_of_element_located(locator))
             print("Page is ready!")
         except TimeoutException as e:
-            print("Loading took too much time!",e)
+            print("Loading took too much time!", e)
 
     def click_operation(self, locator):
         WebDriverWait(self.driver, 30).until(expected_conditions.visibility_of_element_located(locator)).click()
 
     def send_keys_operation(self, locator, keys):
         WebDriverWait(self.driver, 30).until(expected_conditions.visibility_of_element_located(locator)).send_keys(keys)
+
+    def send_keys_js(self, locator, loc, keys):
+        WebDriverWait(self.driver, 30).until(expected_conditions.visibility_of_element_located(locator))
+        #self.driver.executeScript("document.getElementByClassName('" + loc + "').setAttribute('value', '" + keys + "')")
+        javaScript = "document.getElementsByClassName('" + loc + "')[0].value = '" + keys + "' "
+        self.driver.execute_script(javaScript)
 
     def get_text_from_locator(self, locator):
         text = WebDriverWait(self.driver, 30).until(expected_conditions.visibility_of_element_located(locator)).text
@@ -31,3 +37,7 @@ class Base_Page:
         text = WebDriverWait(self.driver, 30).until(
             expected_conditions.visibility_of_element_located(locator)).get_attribute(attribute)
         return text
+
+    def get_title(self):
+        get_title = self.driver.title
+        return get_title
--- Pages/Login_Page.py
+++ Pages/Login_Page.py
@@ -1,6 +1,11 @@
-from selenium.webdriver.common.by import By
+import time
 
+import pytest
+import unittest
+from selenium.webdriver.common.by import By
+from Config.config import Test_Data
 from Pages.Base_Page import Base_Page
+from Testscripts.test_Base import test_Base
 
 
 class Login_Page(Base_Page):
@@ -8,10 +13,32 @@ class Login_Page(Base_Page):
     EMAIL_ADDRESS = (By.XPATH, "// input[ @ class ='_2IX_2- VJZDxU']")
     PASSWORD = (By.XPATH, "// input[@ class ='_2IX_2- _3mctLh VJZDxU']")
     LOGIN_BUTTON = (By.XPATH, "// button[@ class ='_2KpZ6l _2HKlqd _3AWRsL']")
+    EMAIL_CLASSNAME = "_2IX_2- VJZDxU"
+    PASSWORD_CLASSNAME = "_2IX_2- _3mctLh VJZDxU"
+    GET_USER_NAME = (By.XPATH,
+                     "//*[text()='More']/ancestor::div[@class='go_DOp']//preceding-sibling::div[@class='go_DOp']//div[@class='exehdJ']")
+
+
 
     def __init__(self, driver):
         super().__init__(driver)
 
+
     def test_login_to_application(self):
-        self.enter_url_operation("https://www.flipkart.com/", Login_Page.LOGIN_POPUP)
-        self.send_keys_operation(Login_Page.EMAIL_ADDRESS, "alfotech512@gmail.com")
+        log = test_Base.getLogger()
+        dict_d = {}
+        log.info("Getting Test Data")
+        dict_d = Test_Data.getTestData(self, "test_Login")
+        log.info("Entering the url")
+        self.enter_url_operation(dict_d["url"], Login_Page.LOGIN_POPUP)
+        log.info("Entering the Username")
+        self.send_keys_operation(Login_Page.EMAIL_ADDRESS, dict_d["username"])
+        log.info("Entering the Password")
+        self.send_keys_operation(Login_Page.PASSWORD, dict_d["password"])
+        log.info("Clicking login button")
+        self.click_operation(Login_Page.LOGIN_BUTTON)
+        user = self.get_text_from_locator(Login_Page.GET_USER_NAME)
+        log.info("Validating if the user is successfully logged in")
+        assert user == dict_d["loggedinuser"]
+        log.info("user is successfully logged in the account")
+
